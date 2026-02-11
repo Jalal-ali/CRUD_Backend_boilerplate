e6373809af0be558e769a040af05df652ebf2370
Jalal Ali <alikhanumar795@gmail.com>
2026-02-11 18:16:16 +0500

    package installed

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import CrudRoutes from './routes/crud.routes.js'
 import connectDB from './db/index.js';
 const app = express()
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello umar!')
-})
 app.use("/api/v2", CrudRoutes);
 connectDB() ;
 
--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
 dotenv.config();
+
 const uri = process.env.URI ;
 const connectDB = async() => {
     await mongoose.connect(uri)
