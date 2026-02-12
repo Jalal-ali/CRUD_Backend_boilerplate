c57752ff79206b46ad59e51037ca3c917927d915
Jalal Ali <alikhanumar795@gmail.com>
2026-02-12 18:44:10 +0500

    update function edited

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import CrudRoutes from './routes/crud.routes.js'
 import connectDB from './db/index.js';
 const app = express()
-app.use(cors());
+app.use(cors({origin : '*'}));
 app.use(express.json());
 
 app.use("/api/v2", CrudRoutes);
--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000 ;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0' , () => {
   console.log(`Example app listening on port ${PORT}`)
 })
